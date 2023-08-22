79acb906a4419673ce65956a8f1bd086797a3872
rizviabbas128 <108932526+rizviabbas128@users.noreply.github.com>
2023-08-22 19:26:42 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,38 @@ const app: Application = express();
 app.use(express.json());
 
 interface CustomerRecord {
+    id: string;
+    domain: string;
+    direction: string;
+    text: string;
     userId: number;
     fromName: string;
     from_num: string;
     timestamp: string;
     type: string;
+    toName: string;
 }
+
+app.get('/masterdata', (req: Request, res: Response) => {
+    const CustomerData = customerRecords.map((item: CustomerRecord) => {
+        return {
+            id: item.id,
+            type: item.type,
+            domain: item.domain,
+            direction: item.direction,
+            from_num: item.from_num,
+            text: item.text,
+            fromName: item.fromName,
+            userId: item.userId,
+            toName: item.toName,
+            timestamp: item.timestamp,
+        }
+    })
+    return res.status(200).json({
+        CustomerData
+    })
+})
+
 
 app.get('/callrecord', (req: Request, res: Response) => {
 
